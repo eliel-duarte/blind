a64c8d7b90932cbebab2f8bebc15538bdeda0c7b
Eliel Duarte <eliel.duarte@pmpratania.com>
2023-05-02 15:15:01 -0300

    blind

--- painel.php
+++ painel.php
@@ -8,7 +8,7 @@
     //echo $Tempo->subtrairTempo( "1986/09/25", "2023/09/25" )
 
     $inicioTorneio = strtotime( '2023-05-02 14:00:00' );
-    $agora = strtotime( date() );
+    $agora = strtotime( date('d/m/Y H:i:s') );
     $diferenca = $agora - $inicioTorneio;
     
     echo date('H:i:s', $diferenca);
