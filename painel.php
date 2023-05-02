11d6e303304758272cf3869ad282f4650eecd30e
Eliel Duarte <eliel.duarte@pmpratania.com>
2023-05-02 15:12:15 -0300

    blind

--- painel.php
+++ painel.php
@@ -7,7 +7,7 @@
     echo "<br />";
     //echo $Tempo->subtrairTempo( "1986/09/25", "2023/09/25" )
 
-    $entrada = strtotime( '209-05-25 23:30:15' );
+    $entrada = strtotime( '2009-05-25 23:30:15' );
     $saida   = strtotime( '2010-05-26 11:15:20' );
     $diferenca = $saida - $entrada;
     
