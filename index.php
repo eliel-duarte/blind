416c8ab90cc8e6039b8f154ee59c8984c0ac4fac
Eliel Duarte <eliel.duarte@pmpratania.com>
2023-05-02 14:26:16 -0300

    blind

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- METAS -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- PROPIEDADES -->
+    <title>Painel - Poker</title>
 
-    echo "oi";
+    <!-- SCRIPTS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>
+        setInterval(function (){
+            $( "#container" ).load( "painel.php" );
+        }, 6000);
+    </script>
 
-?>
+    <!-- ESTILOS -->
+
+</head>
+<body>
+    <div id="container"></div>
+    
+</body>
+</html>
