19b34a664411a4b7a4140956258af68acbf684f9
Eliel Duarte <eliel.duarte@pmpratania.com>
2023-05-03 16:31:03 -0300

    tempo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     date_default_timezone_set('America/Sao_Paulo');
 
     require_once("../config.php");
-    require_once("classes/Conexao.class.php");
+    //require_once("classes/Conexao.class.php");
     require_once("classes/Tempo.class.php");
 
 ?>
