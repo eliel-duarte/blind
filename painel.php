0a3fada1854d266b544e9a2905f523aa6f0e1d25
Eliel Duarte <eliel.duarte@pmpratania.com>
2023-05-03 14:17:49 -0300

    tempo

--- painel.php
+++ painel.php
@@ -3,12 +3,20 @@
     require_once("config.php");
 
     // Data e Hora de inicio do Torneio
-    $inicioTorneio = date("2023-05-01 22:00:00");
+    $inicioTorneio = date("2023-05-03 14:15:00");
     
     // Data e hora atual
     echo $agora = date("Y-m-d H:i:s");
     echo "<br>";
     // Tempo percorrido
     echo $Tempo->tempoPercorrido( $agora , $inicioTorneio );
+
+    $tempoBlind = date("00:15:00");
+    $tptempoBlind = strtotime($tempoBlind);
+    $tpInicio = strtotime($inicio);
+    $tempoRestante = $tptempoBlind - $tpInicio;
+    gmdate("H:i:s", $tempoRestante);
+    
+
 
 ?>
