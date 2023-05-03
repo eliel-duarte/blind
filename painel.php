27acce3e027d94ca4d9514caa1346e20669ba6f3
Eliel Duarte <eliel.duarte@pmpratania.com>
2023-05-03 14:12:09 -0300

    tempo

--- painel.php
+++ painel.php
@@ -3,7 +3,7 @@
     require_once("config.php");
 
     // Data e Hora de inicio do Torneio
-    $inicioTorneio = date("2023-05-01 23:00:00");
+    $inicioTorneio = date("2023-05-01 22:00:00");
     
     // Data e hora atual
     echo $agora = date("Y-m-d H:i:s");
