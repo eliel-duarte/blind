20795e0a60c78b88709e41e35bf26fdfaa18c918
Eliel Duarte <eliel.duarte@pmpratania.com>
2023-05-03 14:45:27 -0300

    tempo

--- classes/Tempo.class.php
+++ classes/Tempo.class.php
@@ -2,6 +2,7 @@
 
     class Tempo
     {
+        // Função que calcula o tempo percorrido do torneio
         function tempoPercorrido( $agora, $inicio )
         {            
             // Converter $agora para o formato Unix timestamp
@@ -16,6 +17,18 @@
             // Retorna no fomato H:m:s
             return gmdate("H:i:s", $tempoPercorrido);
 
+        }
+
+        // funcção que calcula o tempo restante do blind
+        function tempoBlindRestante( $tempoBlind )
+        {
+            // divide o tempo em array
+            $tempo = explode(":", $tempoBlind);
+
+            // timestamp formula = (horas*3600) + (minutos*60) + segundos
+            $tpTempo = (($tempo[0]*3600) + ($tempo[1]*60) + $tempo[2]);
+
+            return $tpTempo; 
         }
 
     }
--- painel.php
+++ painel.php
@@ -8,11 +8,15 @@
     // Data e hora atual
     $agora = date("Y-m-d H:i:s");
     echo "Agora: ".$agora."<br>";
+
     // Tempo percorrido
     echo "Percorrido: ".$Tempo->tempoPercorrido( $agora , $inicioTorneio )."<br>";
     
-    $tempoBlind = date("0:15:00");
-    echo $tpTempoBlind = strtotime($tempoBlind);
+    // tempo do blind restante
+    echo $Tempo->tempoBlindRestante("00:15:00");
+
+    //$tempoBlindHoras = 
+    //echo $tpTempoBlind = strtotime($tempoBlind);
     //$tpAgora = strtotime($agora);
     //$tpInicio = strtotime($inicioTorneio);
     //$tempoRestante = $tptempoBlind - ( $tpAgora - $tpInicio);
