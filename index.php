92b866261ca2834c6a68b735643a3a48adcdc478
Eliel Duarte <elielduarte@hotmail.com>
2024-09-11 23:47:50 -0300

    comentários das alterações

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- PROPIEDADES -->
-    <title>Painel - Poker</title>
+    <title>Painel - PokerXX</title>
 
     <!-- SCRIPTS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
