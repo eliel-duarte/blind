8adc9275b5e39706569cde5a3296ecf72028806f
Eliel Duarte <elielduarte@hotmail.com>
2024-09-11 23:56:52 -0300

    Reciclagem

--- api/index.php
+++ api/index.php
@@ -7,27 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- PROPIEDADES -->
-    <title>Painel - PokerXX</title>
+    <title>Painel - Poker</title>
 
     <!-- SCRIPTS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>
-        setInterval(function (){
-            $( "#container" ).load( "painel.php" );
-        }, 1000);
-
-        function pausaTorneio(){
-            $( "#console" ).load( "pausaTorneio.php" );            
-        }
-    </script>
 
     <!-- ESTILOS -->
 
 </head>
 <body>
-    <div id="console"></div>
-    <br />
-    <div id="container"></div>    
+    
+    <?php
+        phpinfo();
+    ?>   
     
 </body>
 </html>
